e6a8fde94c43a679389dab63ae03c7ffd66447b7
elpava <elpavac@gmail.com>
2024-03-14 13:00:24 +0330

    add database connection

---
+++ src/database/connect.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb'
+
+const DATABASE_NAME = process.env.DB_NAME
+const PRODUCTS_COLLECTION = 'products'
+const USERS_COLLECTION = 'users'
+const ORDERS_COLLECTION = 'orders'
+const URI = 'mongodb://0.0.0.0:27017/'
+
+const client = new MongoClient(URI)
+
+async function connectToDatabase(caller) {
+  try {
+    await client.connect()
+    console.log(`🆕 [${caller}]: connect to the database.`)
+  } catch (error) {
+    console.log(`❌ [${caller}]: couldn't connect to the database.`)
+  }
+}
+
+export {
+  DATABASE_NAME,
+  PRODUCTS_COLLECTION,
+  USERS_COLLECTION,
+  ORDERS_COLLECTION,
+  client,
+  connectToDatabase,
+}
