72e2b5fe5fd78c19a9c5516cdfc887a3c5e9488e
elpava <elpavac@gmail.com>
2024-06-23 11:02:47 +0330

    remove additional value from matcher in middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)', '/profile'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
 
 export { auth as middleware } from '@/auth'
