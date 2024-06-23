48e28d5b35221ce1bc4956367d4a6b3e2584dc96
elpava <elpavac@gmail.com>
2024-06-23 11:14:28 +0330

    improve animation in ProductsTabelContent

--- src/app/_components/ui/products-table-content.js
+++ src/app/_components/ui/products-table-content.js
@@ -9,6 +9,8 @@ import { VTF_REDZONE_CLASSIC } from 'util/share-font'
 export default function ProductsTabelContent({ brandsAnchors }) {
   const { observe, entries } = useIntersectionObserver()
   const currentHashRef = React.useRef(null)
+  const brandsAnchorsLength = brandsAnchors.length - 1
+
   if (entries.length > 0) {
     entries.forEach(({ isIntersecting, target }) => {
       if (isIntersecting && target.id !== currentHashRef.current) {
@@ -26,27 +28,33 @@ export default function ProductsTabelContent({ brandsAnchors }) {
 
   return (
     <nav
-      className={`text-1xl fixed left-0 ml-4 flex h-svh flex-col items-center justify-center space-y-2 md:z-40 md:text-6xl ${VTF_REDZONE_CLASSIC.className}`}
+      className={`text-1xl fixed left-0 ml-4 flex h-svh flex-col items-center justify-center space-y-2 md:z-40 md:ml-6 md:text-5xl ${VTF_REDZONE_CLASSIC.className}`}
     >
-      {brandsAnchors.map(({ name, href }) => (
+      {brandsAnchors.map(({ name, href }, idx) => (
         <React.Fragment key={name}>
           <Link
             href={`/products${href}`}
             className={clsx(
-              'border-b-2 border-transparent transition-[border] hover:border-b-2 hover:border-lime-400 md:pb-1',
-              currentHashRef.current === href && '!border-lime-400',
+              'border-b-2 border-transparent transition-transform md:hover:scale-105',
+              currentHashRef.current === href && 'scale-100 font-bold',
+              currentHashRef.current !== href && 'scale-90 text-zinc-400',
             )}
           >
             {name.toUpperCase()}
           </Link>
 
-          <span
-            className={clsx(
-              'h-10 w-0.5 rounded-md bg-zinc-700 md:h-16',
-              currentHashRef.current === href &&
-                'animate-scroll bg-gradient-to-b from-lime-400 to-lime-400 bg-[length:100%_5%] bg-no-repeat transition-[background-size]',
-            )}
-          />
+          {idx !== brandsAnchorsLength && (
+            <span
+              className={clsx(
+                'h-10 w-0.5 rounded-md bg-zinc-700 md:h-16',
+                currentHashRef.current === href &&
+                  'animate-scroll bg-gradient-to-b from-lime-400 to-lime-400 bg-[length:100%_5%] bg-no-repeat transition-[background-size]',
+                currentHashRef.current === brandsAnchors.at(-1).href &&
+                  idx === brandsAnchorsLength - 1 &&
+                  'rotate-180 animate-scroll bg-gradient-to-b from-lime-400 to-lime-400 bg-[length:100%_5%] bg-no-repeat transition-[background-size]',
+              )}
+            />
+          )}
         </React.Fragment>
       ))}
     </nav>
