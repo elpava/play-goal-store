c652bd3b1d81f812643d2b1425ccd883b51c36de
elpava <elpavac@gmail.com>
2024-11-24 13:20:16 +0330

    fix scollable page before page transition to prevent transition issue

--- src/library/dom-helper.js
+++ src/library/dom-helper.js
@@ -1,7 +1,10 @@
 import { delay } from './helper-functions'
 
 export async function startPageTransition() {
+  const body = document?.querySelector('body')
   const main = document?.querySelector('main')
+
+  body.style.overflow = 'hidden'
   main.style.opacity = 0
   main.style.transform = 'translateY(32px)'
   main.style.filter = 'blur(8px)'
@@ -9,8 +12,11 @@ export async function startPageTransition() {
 }
 
 export async function endPageTransition() {
+  const body = document?.querySelector('body')
   const main = document?.querySelector('main')
+
   await delay(500)
+  body.style.removeProperty('overflow')
   main.style.removeProperty('opacity')
   main.style.removeProperty('transform')
   main.style.removeProperty('filter')
